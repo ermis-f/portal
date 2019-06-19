66a8402fd4893b11ff1709eb721b8f4c4b6af647
vkopsachilis <vkopsachilis@geo.aegean.gr>
2019-06-19 18:37:19 +0300

    ver2.1

---
+++ portal/portal/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'index.html')
+
+
+def help(request):
+    return render(request,'help.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def workshop(request):
+    return render(request,'workshop.html')
+
